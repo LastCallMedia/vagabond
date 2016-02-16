dc07ad3b885a5123cfccf81c9ef750bc482ccc17
Rob Bayliss <rob@lastcallmedia.com>
2016-02-16 00:22:53 -0500

    Better error checking in running services

--- step/services.go
+++ step/services.go
@@ -4,27 +4,28 @@ import (
 	"os/exec"
 	"fmt"
 	"errors"
+	"bytes"
 )
 
 var ServicesStep = ConfigStep{
 	Name: "service containers",
 	NeedsRun: func(envt *config.Environment) bool {
-		err := dockerCommand(envt, "inspect vagabond_proxy").Run()
-		if err != nil {
+		out, err := dockerCommand(envt, "inspect vagabond_proxy").Output()
+		if err != nil || bytes.Contains(out, []byte("running")) {
 			return true
 		}
-		err = dockerCommand(envt, "inspect vagabond_dnsmasq").Run()
-		return err != nil
+		out, err = dockerCommand(envt, "inspect vagabond_dnsmasq").Output()
+		return err != nil || !bytes.Contains(out, []byte("running"))
 	},
 	Run: func(envt *config.Environment) error {
 		dockerCommand(envt, "stop vagabond_proxy vagabond_dnsmasq").Run()
 		dockerCommand(envt, "rm vagabond_proxy vagabond_dnsmasq").Run()
 		cmd := dockerCommand(envt, "run --name vagabond_proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy")
-		if out, err := cmd.Output(); err != nil {
+		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.New(string(out))
 		}
 		cmd = dockerCommand(envt, fmt.Sprintf("run --name vagabond_dnsmasq -d -p 53:53/udp -p 53:53/tcp --cap-add NET_ADMIN andyshinn/dnsmasq --address=/docker/%s", envt.DockerDaemonIp))
-		if out, err := cmd.Output(); err != nil {
+		if out, err := cmd.CombinedOutput(); err != nil {
 			return errors.New(string(out))
 		}
 		return nil
